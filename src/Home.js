da7fc8bce4c3a00aa48f39dff3fa23862fb6ae24
NithinKumarAlladi <nithinkumaralladi.me@gmail.com>
2019-01-06 23:31:25 +0530

    Added background to the page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,17 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
+import dcb from './imgs/darkCoffeeBeans.jpg'
 
-export default class Home extends Component{
-    render(){
-        return(
+export default class Home extends Component {
+    render() {
+        return (
             <div>
-                this is home page
+                <div style={{ position: 'absolute' }}>
+                    <img src={dcb} style={{ opacity: 0.7, }} />
+                </div>
+                <div>
+                    <p style={{ margin: 0, }}>hello this is a test text</p>
+                </div>
+
             </div>
         )
     }
