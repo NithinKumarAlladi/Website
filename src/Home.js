75d4e6b8064411ba45fd0ae95fede670575fdc49
NithinKumarAlladi <nithinkumaralladi.me@gmail.com>
2019-01-12 23:13:32 +0530

    content modified.

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,9 @@ export default class Home extends Component {
                     <img src={dcb} style={{ opacity: 0.7, }} />
                 </div>
                 <div>
-                    <p style={{ margin: 0, }}>hello this is a test text</p>
+                    <p style={{ margin: 0 }}>Hey there! Want to know something about Nithin kumar Alladi?</p>
+                    <p>Well I'll take you through this weird experience of knowing this guy.</p>
                 </div>
-
             </div>
         )
     }
