38b5fdd4c77b4c04aaa6db37fa8fc743b51a1616
NithinKumarAlladi <nithinkumaralladi.me@gmail.com>
2019-01-15 22:01:13 +0530

    background changed

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,12 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <div style={{ position: 'absolute' }}>
+                {/* <div style={{ position: 'absolute' }}>
                     <img src={dcb} style={{ opacity: 0.7, }} />
-                </div>
-                <div>
-                    <p style={{ margin: 0 }}>Hey there! Want to know something about Nithin kumar Alladi?</p>
-                    <p>Well I'll take you through this weird experience of knowing this guy.</p>
+                </div> */}
+                <div className="backDiv">
+                    <p className="headPara">Hello! Want to know something about Nithin kumar Alladi?
+                    <br />Well I'll take you through this weird experience of knowing this guy.</p>
                 </div>
             </div>
         )
