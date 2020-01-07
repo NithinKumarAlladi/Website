aa620335fdb52d1cf02790464dab4a3f85005ebb
Alladi <170743@NTTDATA.COM>
2020-01-07 17:51:25 +0530

    Added raksgroup images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Home from './Home.js';
 import End from "./End.js";
+import RAKSGroup from './RAKSGroup.js';
+import Nybo from './Nybo.js';
+import Notebook from './Notebook.js';
 
 
 class App extends Component {
@@ -10,15 +13,11 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          {/* <nav className="nav" id="nav">
-            <NavLink to="/" exact activeClassName="activeLink" className="link" >HOME</NavLink>
-            <NavLink to="/about" activeClassName="activeLink" className="link" >ABOUT US</NavLink>
-          </nav> */}
           <div>
             <Route exact path='/' component={Home} />
-            <Route exact path='/RAKSGROUP' component={Home} />
-            <Route exact path='/NYBO' component={Home} />
-            <Route exact path='/NOTEBOOK' component={Home} />
+            <Route exact path='/RAKSGROUP' component={RAKSGroup} />
+            <Route exact path='/NYBO' component={Nybo} />
+            <Route exact path='/NOTEBOOK' component={Notebook} />
           </div>
           <div>
             <End />
---
+++ src/RAKSGroup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class RAKSGroup extends Component {
+    render(){
+        return(
+            <div>
+                <img  />
+            </div>
+        );
+    }
+}
